618670f3f7bf6bc0838c5f0886965fd36c6f1064
Gabriel Osorio <gabriel.osorio.montelius@gmail.com>
2023-06-24 10:39:50 -0300

    Basic MIDI in on Serial1

--- PewBox.ino
+++ PewBox.ino
@@ -1,5 +1,10 @@
 #include <SPI.h>
 #include <Adafruit_SSD1305.h>
+#include <MIDI.h>
+
+// Macro to setup the MIDI Library
+// Uses Serial1, pin 0 (RX1) and 1 (TX1) on the Teensy 3.6
+MIDI_CREATE_DEFAULT_INSTANCE();
 
 // OLED SPI
 #define OLED_CS 10
@@ -42,6 +47,8 @@ void setup() {
   // Make sure serial is online before proceeding
   // while (!Serial) delay(100);
 
+  MIDI.begin(MIDI_CHANNEL_OMNI);
+
   if (!display.begin(0x3C)) {
     Serial.println("Unable to initialize OLED");
     while (1) yield();
@@ -66,6 +73,21 @@ void setup() {
 }
 
 void loop() {
+  if (MIDI.read()) {
+    switch (MIDI.getType()) {
+      case midi::Start:
+        Serial.println("Play");
+      case midi::Stop:
+        Serial.println("Stop");
+      case midi::NoteOn:
+        Serial.println("Note On");
+      case midi::NoteOff:
+        Serial.println("Note Off");
+      default:
+        break;
+    }
+  }
+
   // Sequencer:
   encoder.readEncoderSwitch(*sequencerControlSwitchMomentaryHandler);
   encoder.readEncoderRotation(
